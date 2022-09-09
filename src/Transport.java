f4983ca7d7d16636772035737eb75c3a0023e494
Sergey <zapravilsj@mail.ru>
2022-09-09 23:48:59 +0500

    На проверку

---
+++ src/Transport.java
@@ -0,0 +1,31 @@
+public class Transport {
+    private String modelName;
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    private int wheelsCount;
+
+    public Transport(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
+    }
+
+}
+
+
+
+
